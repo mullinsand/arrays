aa52bc45e4f7896d6d97d8f349354cf17a157b79
Andrew Mullins <mullinsand15@gmail.com>
2022-06-08 08:48:04 -0600

    Call methods on the arrays

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,11 @@ rope=["octopus","tiger","giraffe","emu"]
 whole=[1,2,3,4]
 dotted=[1.1,2.2,3.3,4.4]
 coinflip=[true,true,false,false]
+#pop will remove emu from rope array
+rope.pop
+#push will add 9 to the end of the whole array
+whole.push(9)
+#shift will retrieve and remove 1.1 from dotted array
+dotted.shift
+#unshift will add true to the beginning of coinflip array
+coinflip.unshift(true)
